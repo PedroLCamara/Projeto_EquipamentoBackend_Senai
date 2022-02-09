af04184f6bf49aa9e8a5dff27b9467ddef0991f5
PedroLCamara <plcrlschool@gmail.com>
2022-02-09 16:22:59 -0300

    CriptografiaPronta

---
+++ Patrimonio/Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using Patrimonio.Contexts;
+using Patrimonio.Domains;
+using Patrimonio.Interfaces;
+using Patrimonio.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patrimonio.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private readonly PatrimonioContext ctx;
+
+        public UsuarioRepository(PatrimonioContext appContext)
+        {
+            ctx = appContext;
+        }
+
+        public Usuario Login(string email, string senha)
+        {
+            var usuario = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+
+            if (usuario != null)
+            {
+                if (Criptografia.ValidarCriptografia(usuario.Senha) == true)
+                {
+                    bool confere = Criptografia.Comparar(senha, usuario.Senha);
+                    if (confere) return usuario;
+                }
+                else
+                {
+                    CriptografarSenha(usuario);
+                    bool confere = Criptografia.Comparar(senha, usuario.Senha);
+                    if (confere) return usuario;
+                }
+            }
+
+            return null;
+        }
+
+        public async void CriptografarSenha(Usuario _usuario)
+        {
+            _usuario.Senha = Criptografia.GerarHash(_usuario.Senha);
+            ctx.Usuarios.Update(_usuario);
+            await ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ Patrimonio/Utils/Criptografia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patrimonio.Utils
+{
+    public static class Criptografia
+    {
+        public static string GerarHash(string Senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(Senha);
+        }
+
+        public static bool Comparar(string SenhaForm, string SenhaBanco)
+        {
+            return BCrypt.Net.BCrypt.Verify(SenhaForm, SenhaBanco);
+        }
+
+        public static bool ValidarCriptografia(string SenhaBanco)
+        {
+            if (SenhaBanco.Length == 32 && SenhaBanco.Substring(0, 1) == "$")
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
