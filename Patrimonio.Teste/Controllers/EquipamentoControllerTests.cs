9547d224f3291077c249f5a41abeea6a77b009e2
PedroLCamara <plcrlschool@gmail.com>
2022-02-14 22:30:21 -0300

    TestesBackendComMoqIncompletos

--- Patrimonio.Teste/Controllers/EquipamentoControllerTests.cs
+++ Patrimonio.Teste/Controllers/EquipamentoControllerTests.cs
@@ -75,6 +75,91 @@ namespace Patrimonio.Teste.Controllers
             Assert.IsType<OkObjectResult>(resultado);
         }
 
+        [Fact] //Descricao
+        public void Deve_Retornar_Equipamento_Atualizado()
+        {
+            //Pre-condicao - Arrange
+            Equipamento fakeEquipamento = new Equipamento();
+            int idEquipamento = 1;
+            fakeEquipamento.Id = idEquipamento;
 
+            var fakeRepo = new Mock<IEquipamentoRepository>();
+            fakeRepo.Setup(x => x.Alterar(It.IsAny<Equipamento>()))
+            .Returns(fakeEquipamento);
+
+            var controller = new EquipamentosController(fakeRepo.Object);
+
+            //Procedimento - Act
+            var resultado = controller.PutEquipamento(idEquipamento, fakeEquipamento);
+
+            //Resultado esperado - Assert
+            Assert.IsType<NoContentResult>(resultado);
+        }
+
+        [Fact] //Descricao
+        public void Deve_Retornar_Ids_Incondizentes()
+        {
+            //Pre-condicao - Arrange
+            Equipamento fakeEquipamento = new Equipamento();
+            int idEquipamento = 1;
+            fakeEquipamento.Id = 2;
+
+            var fakeRepo = new Mock<IEquipamentoRepository>();
+            fakeRepo.Setup(x => x.Alterar(It.IsAny<Equipamento>()))
+            .Returns(fakeEquipamento);
+
+            var controller = new EquipamentosController(fakeRepo.Object);
+
+            //Procedimento - Act
+            var resultado = controller.PutEquipamento(idEquipamento, fakeEquipamento);
+
+            //Resultado esperado - Assert
+            Assert.IsType<BadRequestObjectResult>(resultado);
+        }
+
+        [Fact] //Descrição
+        public void Deve_Retornar_Equipamento_Deletar_Nao_Encontrado()
+        {
+            //Pre-condicao - Arrange
+            int idEquipamentoDeletado = 1;
+
+            var fakeRepo = new Mock<IEquipamentoRepository>();
+            fakeRepo.Setup(x => x.BuscarPorID(It.IsAny<int>()))
+                .Returns((Equipamento)null);
+
+            var controller = new EquipamentosController(fakeRepo.Object);
+
+            //Procedimento - Act
+            var resultado = controller.DeleteEquipamento(idEquipamentoDeletado);
+
+            //Resultado esperado - Assert
+            Assert.IsType<NotFoundObjectResult>(resultado);
+        }
+
+        /// <summary>
+        /// Comentado por conta da imagem
+        /// </summary>
+        /*
+        [Fact] //Descrição
+        public void Deve_Retornar_Equipamento_Deletado()
+        {
+            //Pre-condicao - Arrange
+            Equipamento fakeEquipamento = new Equipamento();
+            fakeEquipamento.Id = 1;
+            fakeEquipamento.Imagem = "LETTUCE";
+
+            var fakeRepo = new Mock<IEquipamentoRepository>();
+            fakeRepo.Setup(x => x.BuscarPorID(It.IsAny<int>()))
+            .Returns(fakeEquipamento);
+
+            var controller = new EquipamentosController(fakeRepo.Object);
+
+            //Procedimento - Act
+            var resultado = controller.DeleteEquipamento(fakeEquipamento.Id);
+
+            //Resultado esperado - Assert
+            Assert.IsType<NoContentResult>(resultado);
+        }
+        */
     }
 }
